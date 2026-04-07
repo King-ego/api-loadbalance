cb6d653cedc1e68d90907f70298709d10d254b49
Diego <diegosantos4957@gmail.com>
2026-04-07 13:16:56 -0300

    add REST controller for health check endpoint

--- src/main/java/com/load/balance/controllers/healthController.java
+++ src/main/java/com/load/balance/controllers/healthController.java
@@ -1,4 +1,9 @@
 package com.load.balance.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
 public class healthController {
 }
