af9f18cc28b436d3b29901361914fea98e8a00e6
Diego <diegosantos4957@gmail.com>
2026-04-07 15:27:07 -0300

    add Users entity with UUID identifier and timestamps for creation and update

--- src/main/java/com/load/balance/models/Users.java
+++ src/main/java/com/load/balance/models/Users.java
@@ -1,4 +1,36 @@
 package com.load.balance.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String username;
+    private String password;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
