81e0778f35c5a9f0f62dc25b10ec502a838d1dd2
Diego <diegosantos4957@gmail.com>
2026-04-09 14:47:42 -0300

    add DataSourceConfig class for dynamic data source routing with read and write configurations

--- src/main/java/com/load/balance/configs/DataSourceConfig.java
+++ src/main/java/com/load/balance/configs/DataSourceConfig.java
@@ -1,4 +1,52 @@
 package com.load.balance.configs;
 
+import com.load.balance.enums.DataSourceType;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class DataSourceConfig {
+    @Value("${db.write.url}")
+    private String writeUrl;
+
+    @Value("${db.read.urls}")
+    private String readUrls;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    private HikariDataSource buildDataSource(String url) {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName("org.postgresql.Driver");
+        return ds;
+    }
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        String[] urls = readUrls.split(",");
+
+        Map<Object, Object> targetDataSources = new HashMap<>();
+        targetDataSources.put(DataSourceType.WRITE, buildDataSource(writeUrl));
+        targetDataSources.put(DataSourceType.READ_1, buildDataSource(urls[0].trim()));
+        targetDataSources.put(DataSourceType.READ_2, buildDataSource(urls[1].trim()));
+
+        RoutingDataSource routing =  new RoutingDataSource();
+        routing.setTargetDataSources(targetDataSources);
+        routing.setDefaultTargetDataSource(buildDataSource(writeUrl));
+        return routing;
+    }
 }
