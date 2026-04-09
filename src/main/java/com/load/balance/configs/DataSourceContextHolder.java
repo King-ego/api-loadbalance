b4d5148134af9f8c3fd2abab7204235e7934e125
Diego <diegosantos4957@gmail.com>
2026-04-09 14:16:53 -0300

    add enum for DataSourceType and ThreadLocal context holder in DataSourceContextHolder

--- src/main/java/com/load/balance/configs/DataSourceContextHolder.java
+++ src/main/java/com/load/balance/configs/DataSourceContextHolder.java
@@ -1,4 +1,9 @@
 package com.load.balance.configs;
 
 public class DataSourceContextHolder {
+    private enum DataSourceType {
+        WRITE, READ_1, READ_2
+    }
+
+    private static final ThreadLocal<DataSourceType> contextHolder = new ThreadLocal<>();
 }
