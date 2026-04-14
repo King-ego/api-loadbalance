c1792d8f8b1ad72aec61764c20e8ba21be0db614
Diego <diegosantos4957@gmail.com>
2026-04-14 09:18:11 -0300

    feat: implement UserController with createUser endpoint

--- src/main/java/com/load/balance/controllers/UserController.java
+++ src/main/java/com/load/balance/controllers/UserController.java
@@ -1,4 +1,22 @@
 package com.load.balance.controllers;
 
+import com.load.balance.services.UserServices;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    private final UserServices userService;
+
+    public UserController(UserServices userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public void createUser() {
+        String username = "TED";
+        this.userService.createUser(username);
+    }
 }
