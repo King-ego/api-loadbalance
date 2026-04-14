f465063f8f05203f0e91827356d61d329aeb2a1e
Diego <diegosantos4957@gmail.com>
2026-04-14 10:59:08 -0300

    feat: add getUserName method to retrieve user data by username

--- src/main/java/com/load/balance/controllers/UserController.java
+++ src/main/java/com/load/balance/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.load.balance.controllers;
 
+import com.load.balance.models.Users;
 import com.load.balance.services.UserServices;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +20,11 @@ public class UserController {
     public void createUser() {
         String username = "TED";
         this.userService.createUser(username);
+    }
+
+    @GetMapping
+
+    public Users getUserName() {
+        return this.userService.getUserByUsername("TED");
     }
 }
