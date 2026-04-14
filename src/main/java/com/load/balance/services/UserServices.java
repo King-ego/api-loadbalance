3b2a1c8f2564f182cb8568372bb3c5ded0308e0f
Diego <diegosantos4957@gmail.com>
2026-04-14 09:18:24 -0300

    feat: implement createUser method in UserServices to save new users

--- src/main/java/com/load/balance/services/UserServices.java
+++ src/main/java/com/load/balance/services/UserServices.java
@@ -1,12 +1,26 @@
 package com.load.balance.services;
 
+import com.load.balance.models.Users;
+import com.load.balance.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServices {
+    private final UserRepository userRepository;
+
+    public UserServices(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Transactional(readOnly = false)
     public void createUser(String name) {
+        Users user = Users.builder()
+                .username(name)
+                .password("Ted")
+                .build();
+
+        this.userRepository.save(user);
         System.out.println("Creating user: " + name);
     }
 
