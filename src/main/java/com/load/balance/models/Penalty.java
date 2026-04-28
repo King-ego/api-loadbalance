d9ba71b5e6cf93347435626548b32d11fcb3e4a2
Diego <diegosantos4957@gmail.com>
2026-04-28 08:41:05 -0300

    feat: add UUID id field to Penalty model

--- src/main/java/com/load/balance/models/Penalty.java
+++ src/main/java/com/load/balance/models/Penalty.java
@@ -1,4 +1,18 @@
 package com.load.balance.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Builder
+@Table(name = "penalties")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Penalty {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 }
