b457f981504cf07e75fae05e94a4fac756a2991b
Diego <diegosantos4957@gmail.com>
2026-04-28 08:40:55 -0300

    feat: add UUID id field to Task model

--- src/main/java/com/load/balance/models/Task.java
+++ src/main/java/com/load/balance/models/Task.java
@@ -1,4 +1,21 @@
 package com.load.balance.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tasks")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 }
