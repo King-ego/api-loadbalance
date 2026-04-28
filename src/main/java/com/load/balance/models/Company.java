a305aa83432b8f2a6ee277231e6aeb9855417256
Diego <diegosantos4957@gmail.com>
2026-04-28 08:40:33 -0300

    feat: add UUID id field to Company model

--- src/main/java/com/load/balance/models/Company.java
+++ src/main/java/com/load/balance/models/Company.java
@@ -1,4 +1,21 @@
 package com.load.balance.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Table(name = "companies")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 }
