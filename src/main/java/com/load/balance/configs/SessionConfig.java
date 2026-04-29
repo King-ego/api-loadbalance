8f05483caefd0bebccb030efbcded897036ff695
Diego <diegosantos4957@gmail.com>
2026-04-29 16:26:04 -0300

    feat: add SecurityFilterChain bean for HTTP security configuration

--- src/main/java/com/load/balance/configs/SessionConfig.java
+++ src/main/java/com/load/balance/configs/SessionConfig.java
@@ -1,9 +1,26 @@
 package com.load.balance.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
 public class SessionConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/login", "/auth/logout").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.
+                        ignoringRequestMatchers("/auth/login", "/auth/logout")
+                );
+
+        return http.build();
+    }
 }
