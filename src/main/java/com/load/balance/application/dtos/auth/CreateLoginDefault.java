7450fa8902cbc6ba0cf446e16543dd30b62a576c
Diego <diegosantos4957@gmail.com>
2026-05-01 13:29:48 -0300

    feat: add email and password fields to CreateLoginDefault DTO

--- src/main/java/com/load/balance/application/dtos/auth/CreateLoginDefault.java
+++ src/main/java/com/load/balance/application/dtos/auth/CreateLoginDefault.java
@@ -1,5 +1,13 @@
 package com.load.balance.application.dtos.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateLoginDefault {
+    private String email;
+    private String password;
 }
