672dceb6d87589485acb1908dd98271e700308f7
Diego <diegosantos4957@gmail.com>
2026-05-02 00:47:55 -0300

    feat: enhance Reward entity with Lombok annotations and table mapping

--- src/main/java/com/load/balance/models/Reward.java
+++ src/main/java/com/load/balance/models/Reward.java
@@ -1,13 +1,19 @@
 package com.load.balance.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Entity
+@Builder
+@Table(name = "rewards")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Reward {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
