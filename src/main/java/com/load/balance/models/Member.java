ab90a30c51034d8aeefe0ff981218fc28ede055a
Diego <diegosantos4957@gmail.com>
2026-05-02 16:32:04 -0300

    feat: add Lombok @Builder annotation to Member class

--- src/main/java/com/load/balance/models/Member.java
+++ src/main/java/com/load/balance/models/Member.java
@@ -2,6 +2,7 @@ package com.load.balance.models;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Table(name = "members")
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
