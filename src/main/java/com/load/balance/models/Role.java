ce5697d5b88e3fa9364bde6b3db3f0ebd6687ec3
Diego <diegosantos4957@gmail.com>
2026-05-02 16:06:06 -0300

    feat: add Role entity with UUID identifier and JPA annotations

--- src/main/java/com/load/balance/models/Role.java
+++ src/main/java/com/load/balance/models/Role.java
@@ -1,4 +1,19 @@
 package com.load.balance.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Table(name = "roles")
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 }
