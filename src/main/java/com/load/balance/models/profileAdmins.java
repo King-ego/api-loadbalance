807e43092c2c719d35bb33cdec3b3678b52be1a7
Diego <diegosantos4957@gmail.com>
2026-05-02 16:32:00 -0300

    feat: add JPA annotations and UUID identifier to profileAdmins entity

--- src/main/java/com/load/balance/models/profileAdmins.java
+++ src/main/java/com/load/balance/models/profileAdmins.java
@@ -1,5 +1,19 @@
 package com.load.balance.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "profile_admins")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
 public class profileAdmins {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
 }
