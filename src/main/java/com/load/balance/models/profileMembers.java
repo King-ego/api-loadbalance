7eb2fb50a5c981185188635408244bf099f79c88
Diego <diegosantos4957@gmail.com>
2026-05-02 16:29:14 -0300

    feat: add profileMembers entity with JPA annotations

--- src/main/java/com/load/balance/models/profileMembers.java
+++ src/main/java/com/load/balance/models/profileMembers.java
@@ -1,4 +1,15 @@
 package com.load.balance.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "profile_members")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class profileMembers {
 }
