e4523e323424b789b2c10393b9f222129ae72105
Diego <diegosantos4957@gmail.com>
2026-05-02 16:04:24 -0300

    feat: add Member entity with UUID identifier for load balancing model

--- src/main/java/com/load/balance/models/Member.java
+++ src/main/java/com/load/balance/models/Member.java
@@ -1,4 +1,19 @@
 package com.load.balance.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@Entity
+@Table(name = "members")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 }
