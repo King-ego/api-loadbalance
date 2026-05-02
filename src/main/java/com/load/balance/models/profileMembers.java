3feed22d74cf009f5aa771020e00afc744b209f8
Diego <diegosantos4957@gmail.com>
2026-05-02 16:32:10 -0300

    feat: add Lombok @Builder annotation to profileMembers class

--- src/main/java/com/load/balance/models/profileMembers.java
+++ src/main/java/com/load/balance/models/profileMembers.java
@@ -2,6 +2,7 @@ package com.load.balance.models;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class profileMembers {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
