73a1c3b0fe0beab4e400141b84071c89e1c8b5cf
Diego <diegosantos4957@gmail.com>
2026-05-03 17:21:07 -0300

    feat: extend TaskRepository to use JpaRepository for improved data handling

--- src/main/java/com/load/balance/repositories/TaskRepository.java
+++ src/main/java/com/load/balance/repositories/TaskRepository.java
@@ -1,4 +1,9 @@
 package com.load.balance.repositories;
 
-public interface TaskRepository {
+import com.load.balance.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<Task, UUID> {
 }
