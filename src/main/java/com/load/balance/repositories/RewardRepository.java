c2c5d454cd51238b15df8616d9e0d53a7824e326
Diego <diegosantos4957@gmail.com>
2026-05-03 17:19:44 -0300

    feat: extend RewardRepository to use JpaRepository for improved data handling

--- src/main/java/com/load/balance/repositories/RewardRepository.java
+++ src/main/java/com/load/balance/repositories/RewardRepository.java
@@ -1,4 +1,9 @@
 package com.load.balance.repositories;
 
-public interface RewardRepository {
+import com.load.balance.models.Reward;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RewardRepository extends JpaRepository<Reward, UUID> {
 }
