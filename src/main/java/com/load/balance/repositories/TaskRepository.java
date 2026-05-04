c258742e97d7fba2eed413018735b6a50a5c9dbd
Diego <diegosantos4957@gmail.com>
2026-05-04 15:53:22 -0300

    feat: annotate TaskRepository with @Repository for improved component scanning

--- src/main/java/com/load/balance/repositories/TaskRepository.java
+++ src/main/java/com/load/balance/repositories/TaskRepository.java
@@ -2,8 +2,10 @@ package com.load.balance.repositories;
 
 import com.load.balance.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
 }
