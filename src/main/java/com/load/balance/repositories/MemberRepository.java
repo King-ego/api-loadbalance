930b2529bdd5c79f951b6fe36686408e7f38418b
Diego <diegosantos4957@gmail.com>
2026-05-04 15:52:24 -0300

    feat: extend TaskRepository to use JpaRepository for improved data handling

--- src/main/java/com/load/balance/repositories/MemberRepository.java
+++ src/main/java/com/load/balance/repositories/MemberRepository.java
@@ -2,8 +2,10 @@ package com.load.balance.repositories;
 
 import com.load.balance.models.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, UUID> {
 }
