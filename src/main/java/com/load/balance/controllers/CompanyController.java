fb45927474a0ee596dde17fba4ac78f26941b52a
Diego <diegosantos4957@gmail.com>
2026-05-06 19:20:26 -0300

    feat: update getCompanies endpoint to accept slug parameter for improved company retrieval

--- src/main/java/com/load/balance/controllers/CompanyController.java
+++ src/main/java/com/load/balance/controllers/CompanyController.java
@@ -3,6 +3,7 @@ package com.load.balance.controllers;
 import com.load.balance.models.Company;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
-    @GetMapping()
-    public List<Company> getCompanies(HttpSession session) {
+    @GetMapping("/{slug}")
+    public List<Company> getCompanies(@PathVariable String slu, HttpSession session) {
+        String userId = (String) session.getAttribute("userId");
         return List.of(
                 new Company(),
                 new Company(),
