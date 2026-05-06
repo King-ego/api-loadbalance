5c16de4a08765052e05758719ed326d1ee4a7f36
Diego <diegosantos4957@gmail.com>
2026-05-06 19:17:06 -0300

    feat: add getCompanies endpoint to CompanyController for retrieving company list

--- src/main/java/com/load/balance/controllers/CompanyController.java
+++ src/main/java/com/load/balance/controllers/CompanyController.java
@@ -1,9 +1,22 @@
 package com.load.balance.controllers;
 
+import com.load.balance.models.Company;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
+    @GetMapping()
+    public List<Company> getCompanies(HttpSession session) {
+        return List.of(
+                new Company(),
+                new Company(),
+                new Company()
+        );
+    }
 }
