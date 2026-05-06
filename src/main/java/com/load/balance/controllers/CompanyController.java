e0cd5655b7a0a1d910b7a7daf6ac22267aaed15d
Diego <diegosantos4957@gmail.com>
2026-05-06 19:20:49 -0300

    feat: update getCompanies endpoint to use request parameter for slug

--- src/main/java/com/load/balance/controllers/CompanyController.java
+++ src/main/java/com/load/balance/controllers/CompanyController.java
@@ -2,10 +2,7 @@ package com.load.balance.controllers;
 
 import com.load.balance.models.Company;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,7 +10,7 @@ import java.util.List;
 @RequestMapping("/companies")
 public class CompanyController {
     @GetMapping("/{slug}")
-    public List<Company> getCompanies(@PathVariable String slu, HttpSession session) {
+    public List<Company> getCompanies(@RequestParam String slug, HttpSession session) {
         String userId = (String) session.getAttribute("userId");
         return List.of(
                 new Company(),
