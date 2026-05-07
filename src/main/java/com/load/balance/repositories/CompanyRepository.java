34dfdcbca0fc87dc3165a329371be03f52fe3fb7
Diego <diegosantos4957@gmail.com>
2026-05-07 15:26:19 -0300

    feat: update companiesBySlug query to join with user for accurate retrieval

--- src/main/java/com/load/balance/repositories/CompanyRepository.java
+++ src/main/java/com/load/balance/repositories/CompanyRepository.java
@@ -11,6 +11,6 @@ import java.util.UUID;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, UUID> {
-    @Query("SELECT c FROM Company c WHERE c.slug = :slug AND c.user_id = :user_id")
-    List<Company> companiesBySlug(@RequestParam String slug, String user_id);
+    @Query("SELECT c FROM Company c JOIN c.memberOf u WHERE c.slug = :slug AND u.id = :user_id")
+    List<Company> companiesBySlug(@RequestParam String slug, String userId);
 }
