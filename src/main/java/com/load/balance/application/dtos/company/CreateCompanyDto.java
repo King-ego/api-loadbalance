91e942be7ad9b906075b7d186cef3b392716dbf4
Diego <diegosantos4957@gmail.com>
2026-05-07 15:58:34 -0300

    feat: add non-null constraint to name field in CreateCompanyDto

--- src/main/java/com/load/balance/application/dtos/company/CreateCompanyDto.java
+++ src/main/java/com/load/balance/application/dtos/company/CreateCompanyDto.java
@@ -3,9 +3,14 @@ package com.load.balance.application.dtos.company;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateCompanyDto {
+    @NonNull
+    private String name;
+
+    private String description;
 }
