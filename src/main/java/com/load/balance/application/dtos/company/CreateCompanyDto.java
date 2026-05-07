5fde444e7e5d291fc1ea0c4057a355120efcafc6
Diego <diegosantos4957@gmail.com>
2026-05-07 15:55:55 -0300

    feat: enhance CreateCompanyDto with Lombok annotations for improved code simplicity

--- src/main/java/com/load/balance/application/dtos/company/CreateCompanyDto.java
+++ src/main/java/com/load/balance/application/dtos/company/CreateCompanyDto.java
@@ -1,4 +1,11 @@
 package com.load.balance.application.dtos.company;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateCompanyDto {
 }
