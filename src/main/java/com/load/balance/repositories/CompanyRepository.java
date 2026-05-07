263b4f1d21afa155465a00b5a7b62087dab53e4c
Diego <diegosantos4957@gmail.com>
2026-05-07 15:13:51 -0300

    feat: add custom query method to CompanyRepository for retrieving companies by slug and user ID

--- src/main/java/com/load/balance/repositories/CompanyRepository.java
+++ src/main/java/com/load/balance/repositories/CompanyRepository.java
@@ -2,10 +2,15 @@ package com.load.balance.repositories;
 
 import com.load.balance.models.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, UUID> {
+    @Query("SELECT c FROM Company c WHERE c.slug = :slug AND c.user_id = :user_id")
+    List<Company> companiesBySlug(@RequestParam String slug, String user_id);
 }
