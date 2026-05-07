eeb2330d6650895987e28eac124099d3d7ce3ecb
Diego <diegosantos4957@gmail.com>
2026-05-07 16:41:40 -0300

    feat: add createCompany method to CompanyServices for company creation

--- src/main/java/com/load/balance/services/CompanyServices.java
+++ src/main/java/com/load/balance/services/CompanyServices.java
@@ -1,9 +1,12 @@
 package com.load.balance.services;
 
+import com.load.balance.application.dtos.company.CreateCompanyDto;
+import com.load.balance.enums.StatusCompany;
 import com.load.balance.models.Company;
 import com.load.balance.repositories.CompanyRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,8 +18,20 @@ public class CompanyServices {
         this.companyRepository = companyRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<Company> findBySlug(String slug, HttpSession session) {
-         String userId = (String) session.getAttribute("userId");
+        String userId = (String) session.getAttribute("userId");
         return companyRepository.companiesBySlug(slug, userId);
+    }
+
+    @Transactional()
+    public Company createCompany(CreateCompanyDto newCompany, HttpSession session) {
+        String userId = (String) session.getAttribute("userId");
+        Company company = Company.builder()
+                .name(newCompany.getName())
+                .description(newCompany.getDescription())
+                .status(StatusCompany.ACTIVE)
+                .build();
+        return companyRepository.save(company);
     }
 }
