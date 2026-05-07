48cc65b7936238f1516086d3c22c1251c4278ce4
Diego <diegosantos4957@gmail.com>
2026-05-07 15:46:49 -0300

    feat: implement findBySlug method in CompanyServices for retrieving companies by slug and user ID

--- src/main/java/com/load/balance/services/CompanyServices.java
+++ src/main/java/com/load/balance/services/CompanyServices.java
@@ -1,4 +1,22 @@
 package com.load.balance.services;
 
+import com.load.balance.models.Company;
+import com.load.balance.repositories.CompanyRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CompanyServices {
+    private final CompanyRepository companyRepository;
+
+    public CompanyServices(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public List<Company> findBySlug(String slug, HttpSession session) {
+         String userId = (String) session.getAttribute("userId");
+        return companyRepository.companiesBySlug(slug, userId);
+    }
 }
