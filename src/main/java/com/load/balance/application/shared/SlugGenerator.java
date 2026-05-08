75407a553ef2b27d267759cf14a5c3ecbb30aa7f
Diego <diegosantos4957@gmail.com>
2026-05-08 00:07:32 -0300

    feat: add generate method to SlugGenerator for slug creation

--- src/main/java/com/load/balance/application/shared/SlugGenerator.java
+++ src/main/java/com/load/balance/application/shared/SlugGenerator.java
@@ -1,4 +1,14 @@
 package com.load.balance.application.shared;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SlugGenerator {
+    public String generate(String name) {
+        return name.toLowerCase()
+                .trim()
+                .replaceAll("[^a-z0-9\\s-]", "")
+                .replaceAll("\\s+", "-")
+                .replaceAll("-+", "-");
+    }
 }
