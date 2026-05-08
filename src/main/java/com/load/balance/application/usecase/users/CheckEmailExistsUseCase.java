ece41a5ad7be784aa905039f8bb0a81267a1d30f
Diego <diegosantos4957@gmail.com>
2026-05-08 19:30:16 -0300

    feat: implement CheckEmailExistsUseCase for email existence verification

--- src/main/java/com/load/balance/application/usecase/users/CheckEmailExistsUseCase.java
+++ src/main/java/com/load/balance/application/usecase/users/CheckEmailExistsUseCase.java
@@ -1,4 +1,23 @@
 package com.load.balance.application.usecase.users;
 
+import com.load.balance.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class CheckEmailExistsUseCase {
+    private final UserRepository userRepository;
+
+    public CheckEmailExistsUseCase(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void execute(String email) {
+        this.userRepository.findByEmail(email)
+                .orElseThrow(() -> {
+                    log.info("User with email {} not found", email);
+                    return new RuntimeException("User not found");
+                });
+    }
 }
