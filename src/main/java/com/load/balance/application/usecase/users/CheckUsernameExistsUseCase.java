ef5bf374dbb06484ea1cf7c48ba5109cd4efe979
Diego <diegosantos4957@gmail.com>
2026-05-08 19:30:22 -0300

    feat: implement CheckUsernameExistsUseCase for username existence verification

--- src/main/java/com/load/balance/application/usecase/users/CheckUsernameExistsUseCase.java
+++ src/main/java/com/load/balance/application/usecase/users/CheckUsernameExistsUseCase.java
@@ -1,4 +1,22 @@
 package com.load.balance.application.usecase.users;
 
+import com.load.balance.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class CheckUsernameExistsUseCase {
+    private UserRepository userRepository;
+    public CheckUsernameExistsUseCase(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void execute(String username) {
+        this.userRepository.findByUsername(username)
+                .orElseThrow(() -> {
+                    log.info("User with username {} not found", username);
+                    return new RuntimeException("User not found");
+                });
+    }
 }
