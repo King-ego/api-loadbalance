327d494566d39d2b8bce2e11a458df8c120c1eae
Diego <diegosantos4957@gmail.com>
2026-05-08 00:07:37 -0300

    feat: enhance createCompany method to include slug generation and user validation

--- src/main/java/com/load/balance/services/CompanyServices.java
+++ src/main/java/com/load/balance/services/CompanyServices.java
@@ -1,21 +1,34 @@
 package com.load.balance.services;
 
 import com.load.balance.application.dtos.company.CreateCompanyDto;
+import com.load.balance.application.shared.SlugGenerator;
 import com.load.balance.enums.StatusCompany;
 import com.load.balance.models.Company;
+import com.load.balance.models.Users;
 import com.load.balance.repositories.CompanyRepository;
+import com.load.balance.repositories.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CompanyServices {
     private final CompanyRepository companyRepository;
+    private final SlugGenerator slugGenerator;
+    private final UserRepository userRepository;
 
-    public CompanyServices(CompanyRepository companyRepository) {
+    public CompanyServices(
+            CompanyRepository companyRepository,
+            SlugGenerator slugGenerator,
+            UserRepository userRepository
+    ) {
         this.companyRepository = companyRepository;
+        this.slugGenerator = slugGenerator;
+        this.userRepository = userRepository;
     }
 
     @Transactional(readOnly = true)
@@ -26,11 +39,23 @@ public class CompanyServices {
 
     @Transactional()
     public Company createCompany(CreateCompanyDto newCompany, HttpSession session) {
-        String userId = (String) session.getAttribute("userId");
+        UUID userId = (UUID) session.getAttribute("userId");
+
+
+        Optional<Users> user = this.userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            throw new RuntimeException("User not found");
+        }
+
+        Users userDetails = user.get();
+
         Company company = Company.builder()
                 .name(newCompany.getName())
                 .description(newCompany.getDescription())
                 .status(StatusCompany.ACTIVE)
+                .slug(slugGenerator.generate(newCompany.getName()))
+                .createdBy(userDetails)
                 .build();
         return companyRepository.save(company);
     }
