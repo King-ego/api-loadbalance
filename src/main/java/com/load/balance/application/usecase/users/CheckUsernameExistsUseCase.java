d9fe03d832ae9dc1ce61e162d529756d92cc1639
Diego <diegosantos4957@gmail.com>
2026-05-09 00:07:44 -0300

    feat: enhance CheckUsernameExistsUseCase with methods for username existence checks

--- src/main/java/com/load/balance/application/usecase/users/CheckUsernameExistsUseCase.java
+++ src/main/java/com/load/balance/application/usecase/users/CheckUsernameExistsUseCase.java
@@ -7,16 +7,23 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class CheckUsernameExistsUseCase {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
     public CheckUsernameExistsUseCase(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public void execute(String username) {
+    public void exist(String username) {
         this.userRepository.findByUsername(username)
                 .orElseThrow(() -> {
                     log.info("User with username {} not found", username);
                     return new RuntimeException("User not found");
                 });
+    }
+
+    public void notExist(String username) {
+        this.userRepository.findByUsername(username).ifPresent(user -> {;
+            log.info("User with username {} already exists", username);
+            throw new RuntimeException("User already exists");
+        });
     }
 }
