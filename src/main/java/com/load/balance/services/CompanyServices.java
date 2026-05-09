3a9f6ac06dada89932096bb93ac8e0d545c597fe
Diego <diegosantos4957@gmail.com>
2026-05-09 00:07:57 -0300

    feat: refactor CompanyServices to use FindUserOrThrowUseCase for user retrieval

--- src/main/java/com/load/balance/services/CompanyServices.java
+++ src/main/java/com/load/balance/services/CompanyServices.java
@@ -2,33 +2,32 @@ package com.load.balance.services;
 
 import com.load.balance.application.dtos.company.CreateCompanyDto;
 import com.load.balance.application.shared.SlugGenerator;
+import com.load.balance.application.usecase.users.FindUserOrThrowUseCase;
 import com.load.balance.enums.StatusCompany;
 import com.load.balance.models.Company;
 import com.load.balance.models.Users;
 import com.load.balance.repositories.CompanyRepository;
-import com.load.balance.repositories.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class CompanyServices {
     private final CompanyRepository companyRepository;
     private final SlugGenerator slugGenerator;
-    private final UserRepository userRepository;
+    private final FindUserOrThrowUseCase findUserOrThrowUseCase;
 
     public CompanyServices(
             CompanyRepository companyRepository,
             SlugGenerator slugGenerator,
-            UserRepository userRepository
+            FindUserOrThrowUseCase findUserOrThrowUseCase
     ) {
         this.companyRepository = companyRepository;
         this.slugGenerator = slugGenerator;
-        this.userRepository = userRepository;
+        this.findUserOrThrowUseCase = findUserOrThrowUseCase;
     }
 
     @Transactional(readOnly = true)
@@ -40,22 +39,15 @@ public class CompanyServices {
     @Transactional()
     public Company createCompany(CreateCompanyDto newCompany, HttpSession session) {
         UUID userId = (UUID) session.getAttribute("userId");
-
-
-        Optional<Users> user = this.userRepository.findById(userId);
-
-        if (user.isEmpty()) {
-            throw new RuntimeException("User not found");
-        }
-
-        Users userDetails = user.get();
+        Users creator = this.findUserOrThrowUseCase.execute(userId);
+        String slug = this.slugGenerator.generate(newCompany.getName());
 
         Company company = Company.builder()
                 .name(newCompany.getName())
                 .description(newCompany.getDescription())
                 .status(StatusCompany.ACTIVE)
-                .slug(slugGenerator.generate(newCompany.getName()))
-                .createdBy(userDetails)
+                .slug(slug)
+                .createdBy(creator)
                 .build();
         return companyRepository.save(company);
     }
