c110deaacb07e0953d94059a67723ee94defbb1c
Diego <diegosantos4957@gmail.com>
2026-05-09 00:07:39 -0300

    feat: update UserRepository to return Optional for findByUsername and findByEmail methods

--- src/main/java/com/load/balance/repositories/UserRepository.java
+++ src/main/java/com/load/balance/repositories/UserRepository.java
@@ -7,13 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<@NonNull Users, @NonNull UUID> {
     @Query("SELECT u FROM Users u WHERE u.username = :username")
-    Users findByUsername(@Param("username") String username);
+    Optional<Users> findByUsername(@Param("username") String username);
 
     @Query("SELECT u FROM Users u WHERE u.email = :email")
-    Users findByEmail(String email);
+    Optional<Users> findByEmail(String email);
 }
