d2bb755912bcbdbdee8bae678e60ad27b6305b42
Diego <diegosantos4957@gmail.com>
2026-05-09 00:08:49 -0300

    feat: implement password validation logic in ValidatePasswordUseCase

--- src/main/java/com/load/balance/application/usecase/users/ValidatePasswordUseCase.java
+++ src/main/java/com/load/balance/application/usecase/users/ValidatePasswordUseCase.java
@@ -1,4 +1,14 @@
 package com.load.balance.application.usecase.users;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ValidatePasswordUseCase {
+    public ValidatePasswordUseCase() {}
+
+    public void execute(String password, String confirmPassword) {
+        if (!password.equals(confirmPassword)) {
+            throw new RuntimeException("Passwords do not match");
+        }
+    }
 }
