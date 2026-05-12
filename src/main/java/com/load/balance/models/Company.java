90d3af1704fc50e2fe84a4154aeece17a53e6e21
Diego <diegosantos4957@gmail.com>
2026-05-12 20:51:53 -0300

    fix: add JsonBackReference annotation to createdBy field in Company model

--- src/main/java/com/load/balance/models/Company.java
+++ src/main/java/com/load/balance/models/Company.java
@@ -1,5 +1,6 @@
 package com.load.balance.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.load.balance.enums.StatusCompany;
 import jakarta.persistence.*;
 import lombok.*;
@@ -21,6 +22,7 @@ public class Company {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "created_by_id")
+    @JsonBackReference
     private Users createdBy;
 
     @ManyToMany(mappedBy = "memberOf", fetch = FetchType.LAZY)
