9addd944626771be87699126a969626b56b0174b
Diego <diegosantos4957@gmail.com>
2026-05-13 23:15:21 -0300

    feat: add userId and companyId fields to JoinCompanyDTO class

--- src/main/java/com/load/balance/application/dtos/company/JoinCompanyDTO.java
+++ src/main/java/com/load/balance/application/dtos/company/JoinCompanyDTO.java
@@ -1,4 +1,18 @@
 package com.load.balance.application.dtos.company;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class JoinCompanyDTO {
+    @NonNull
+    private UUID userId;
+    @NonNull
+    private String companyId;
 }
