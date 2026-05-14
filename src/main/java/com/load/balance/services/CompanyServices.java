72dd04c45166326b420381dfa1af06e1e2da65d9
Diego <diegosantos4957@gmail.com>
2026-05-13 23:03:46 -0300

    feat: integrate AddMemberAtCompany use case into CompanyServices

--- src/main/java/com/load/balance/services/CompanyServices.java
+++ src/main/java/com/load/balance/services/CompanyServices.java
@@ -2,32 +2,38 @@ package com.load.balance.services;
 
 import com.load.balance.application.dtos.company.CreateCompanyDto;
 import com.load.balance.application.shared.SlugGenerator;
+import com.load.balance.application.usecase.companies.AddMemberAtCompany;
 import com.load.balance.application.usecase.users.FindUserOrThrowUseCase;
 import com.load.balance.enums.StatusCompany;
 import com.load.balance.models.Company;
 import com.load.balance.models.Users;
 import com.load.balance.repositories.CompanyRepository;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CompanyServices {
     private final CompanyRepository companyRepository;
     private final SlugGenerator slugGenerator;
     private final FindUserOrThrowUseCase findUserOrThrowUseCase;
+    private final AddMemberAtCompany addMemberAtCompany;
 
     public CompanyServices(
             CompanyRepository companyRepository,
             SlugGenerator slugGenerator,
-            FindUserOrThrowUseCase findUserOrThrowUseCase
+            FindUserOrThrowUseCase findUserOrThrowUseCase,
+            AddMemberAtCompany addMemberAtCompany
     ) {
         this.companyRepository = companyRepository;
         this.slugGenerator = slugGenerator;
         this.findUserOrThrowUseCase = findUserOrThrowUseCase;
+        this.addMemberAtCompany = addMemberAtCompany;
     }
 
     @Transactional(readOnly = true)
@@ -51,7 +57,12 @@ public class CompanyServices {
                 .slug(slug)
                 .createdBy(creator)
                 .build();
-        return companyRepository.save(company);
+
+        Company createCompany =  companyRepository.save(company);
+
+        this.addMemberAtCompany.execute(createCompany, creator);
+
+        return createCompany;
     }
 }
 
