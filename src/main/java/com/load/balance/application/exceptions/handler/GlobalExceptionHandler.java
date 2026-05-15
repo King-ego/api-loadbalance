c6ef37d683b2f4a9472ec2d1df26ab57c9585943
Diego <diegosantos4957@gmail.com>
2026-05-15 19:07:41 -0300

    feat: implement GlobalExceptionHandler to handle CustomException and return structured ErrorResponse

--- src/main/java/com/load/balance/application/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/load/balance/application/exceptions/handler/GlobalExceptionHandler.java
@@ -1,4 +1,24 @@
 package com.load.balance.application.exceptions.handler;
 
+import com.load.balance.application.exceptions.base.CustomException;
+import com.load.balance.application.exceptions.base.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Controller
 public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex, HttpServletRequest request) {
+        ErrorResponse error = new ErrorResponse(
+                ex.getStatus().value(),
+                ex.getError(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity
+                .status(ex.getStatus())
+                .body(error);
+    }
 }
