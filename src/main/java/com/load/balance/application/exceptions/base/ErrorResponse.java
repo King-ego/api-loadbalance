c89bc5aebd5ad8f08a50a5a4a24f392f766936c3
Diego <diegosantos4957@gmail.com>
2026-05-15 15:38:16 -0300

    feat: extend ErrorResponse to include status, error details, and timestamp

--- src/main/java/com/load/balance/application/exceptions/base/ErrorResponse.java
+++ src/main/java/com/load/balance/application/exceptions/base/ErrorResponse.java
@@ -1,7 +1,24 @@
 package com.load.balance.application.exceptions.base;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class ErrorResponse extends RuntimeException {
-    public ErrorResponse(String message) {
-        super(message);
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(int status, String error, String message, String path) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+        this.timestamp = LocalDateTime.now();
     }
 }
