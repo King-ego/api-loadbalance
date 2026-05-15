793deb57349e050fffd55df6ccb9c42b1a7ed3b7
Diego <diegosantos4957@gmail.com>
2026-05-15 15:38:21 -0300

    feat: extend UserNotFoundException to inherit from CustomException with HTTP status and error details

--- src/main/java/com/load/balance/application/exceptions/users/UserNotFoundException.java
+++ src/main/java/com/load/balance/application/exceptions/users/UserNotFoundException.java
@@ -1,4 +1,10 @@
 package com.load.balance.application.exceptions.users;
 
-public class UserNotFoundException {
+import com.load.balance.application.exceptions.base.CustomException;
+import org.springframework.http.HttpStatus;
+
+public class UserNotFoundException extends CustomException {
+    public UserNotFoundException() {
+        super(HttpStatus.NOT_FOUND, "User not found", "USER_NOT_FOUND");
+    }
 }
