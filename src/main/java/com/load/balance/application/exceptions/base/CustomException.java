b6d0634efe82146977bd5aff8f734fd831b0af0c
Diego <diegosantos4957@gmail.com>
2026-05-15 15:38:11 -0300

    feat: enhance CustomException with HTTP status and error details

--- src/main/java/com/load/balance/application/exceptions/base/CustomException.java
+++ src/main/java/com/load/balance/application/exceptions/base/CustomException.java
@@ -1,4 +1,26 @@
 package com.load.balance.application.exceptions.base;
 
-public class CustomException {
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+public class CustomException extends RuntimeException {
+    private final HttpStatus status;
+    private final String error;
+
+    public CustomException(HttpStatus httpStatus, String message) {
+        super(message);
+        this.error = "GENERIC_ERROR";
+        this.status = httpStatus;
+    }
+
+    public CustomException(HttpStatus httpStatus, String message, String error) {
+        super(message);
+        this.status = httpStatus;
+        this.error = error;
+    }
+
+
 }
