fbf5f26543555639601dd4cefd0d3c158c5e2f95
Diego <diegosantos4957@gmail.com>
2026-05-16 02:08:24 -0300

    feat: implement FindCompanyOrThrowUseCase to retrieve company by ID with exception handling

--- src/main/java/com/load/balance/application/usecase/companies/FindCompanyOrThrowUseCase.java
+++ src/main/java/com/load/balance/application/usecase/companies/FindCompanyOrThrowUseCase.java
@@ -1,4 +1,20 @@
 package com.load.balance.application.usecase.companies;
 
+import com.load.balance.models.Company;
+import com.load.balance.repositories.CompanyRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
 public class FindCompanyOrThrowUseCase {
+    private final CompanyRepository companyRepository;
+    private FindCompanyOrThrowUseCase(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public Company byId(UUID companyId) {
+        return this.companyRepository.findById(companyId)
+                .orElseThrow(() -> new RuntimeException("Company not found"));
+    }
 }
