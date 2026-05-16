4ef9b56d801d536bf046101803d2b3321b5e766e
Diego <diegosantos4957@gmail.com>
2026-05-16 02:08:18 -0300

    feat: replace RuntimeException with UserNotFoundException and UserAlreadyExists in CheckEmailUseCase

--- src/main/java/com/load/balance/application/usecase/users/CheckEmailUseCase.java
+++ src/main/java/com/load/balance/application/usecase/users/CheckEmailUseCase.java
@@ -1,5 +1,7 @@
 package com.load.balance.application.usecase.users;
 
+import com.load.balance.application.exceptions.users.UserAlreadyExists;
+import com.load.balance.application.exceptions.users.UserNotFoundException;
 import com.load.balance.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -17,7 +19,7 @@ public class CheckEmailUseCase {
         this.userRepository.findByEmail(email)
                 .orElseThrow(() -> {
                     log.info("User with email {} not found", email);
-                    return new RuntimeException("User not found");
+                    return new UserNotFoundException();
                 });
     }
 
@@ -25,7 +27,7 @@ public class CheckEmailUseCase {
         this.userRepository.findByEmail(email)
                 .ifPresent(user -> {
                     log.info("User with email {} already exists", email);
-                    throw new RuntimeException("User already exists");
+                    throw new UserAlreadyExists();
                 });
     }
 }
