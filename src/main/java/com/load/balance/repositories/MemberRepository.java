8d53ea72ae70742b1560009fba136031833765d6
Diego <diegosantos4957@gmail.com>
2026-05-16 02:08:50 -0300

    feat: add custom query to MemberRepository for finding member by user and company IDs

--- src/main/java/com/load/balance/repositories/MemberRepository.java
+++ src/main/java/com/load/balance/repositories/MemberRepository.java
@@ -2,10 +2,14 @@ package com.load.balance.repositories;
 
 import com.load.balance.models.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, UUID> {
+    @Query("SELECT m FROM Member m WHERE m.user.id = :userId AND m.company.id = :companyId")
+    Optional<Member> findByUserIdAndCompanyId(UUID userId, UUID companyId);
 }
