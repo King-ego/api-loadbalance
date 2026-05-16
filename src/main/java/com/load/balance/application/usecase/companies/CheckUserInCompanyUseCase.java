5c918de24d80a6b7389b7a64832b43f4584dbea0
Diego <diegosantos4957@gmail.com>
2026-05-16 02:08:38 -0300

    feat: implement CheckUserInCompanyUseCase to verify user membership in a company

--- src/main/java/com/load/balance/application/usecase/companies/CheckUserInCompanyUseCase.java
+++ src/main/java/com/load/balance/application/usecase/companies/CheckUserInCompanyUseCase.java
@@ -1,4 +1,27 @@
 package com.load.balance.application.usecase.companies;
 
+import com.load.balance.repositories.MemberRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
 public class CheckUserInCompanyUseCase {
+    private final MemberRepository memberRepository;
+
+    public CheckUserInCompanyUseCase(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void exist(UUID userId, UUID memberId) {
+        this.memberRepository.findByUserIdAndCompanyId(userId, memberId)
+                .orElseThrow(() -> new RuntimeException("User is not a member of the company"));
+    }
+
+    public void notExist(UUID userId, UUID memberId) {
+        this.memberRepository.findByUserIdAndCompanyId(userId, memberId)
+                .ifPresent(member -> {
+                    throw new RuntimeException("User is already a member of the company");
+                });
+    }
 }
