ebac097769b3a7e0226076c741f7fdbe1d239ae7
Diego <diegosantos4957@gmail.com>
2026-05-17 22:06:09 -0300

    feat: add RemoveJoinCompanyDTO with companyId and userId fields

--- src/main/java/com/load/balance/application/dtos/company/RemoveJoinCompanyDTO.java
+++ src/main/java/com/load/balance/application/dtos/company/RemoveJoinCompanyDTO.java
@@ -1,4 +1,19 @@
 package com.load.balance.application.dtos.company;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RemoveJoinCompanyDTO {
+    @NonNull
+    private UUID companyId;
+
+    @NonNull
+    private UUID userId;
 }
