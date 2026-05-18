7eb651a27c31b1f956499bd74b03d8f9e5a1a465
Diego <diegosantos4957@gmail.com>
2026-05-18 10:29:55 -0300

    feat: enhance Task entity with additional fields and lifecycle callbacks

--- src/main/java/com/load/balance/models/Task.java
+++ src/main/java/com/load/balance/models/Task.java
@@ -1,11 +1,13 @@
 package com.load.balance.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -18,4 +20,38 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    private String name;
+
+    private String description;
+
+    private String status;
+
+    private Integer priority;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private int completedIn;
+    private LocalDateTime completedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
