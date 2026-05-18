90da55a526373b23e2d49011a00bc330f6927da2
Diego <diegosantos4957@gmail.com>
2026-05-18 10:32:18 -0300

    feat: rename Company class to Companies for improved clarity

--- src/main/java/com/load/balance/models/Companies.java
+++ src/main/java/com/load/balance/models/Companies.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Getter
 @Builder
-public class Company {
+public class Companies {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
