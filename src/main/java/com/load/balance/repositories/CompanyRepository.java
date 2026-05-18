dee41c27a73586ffa3cce1da8df08551f2a6b042
Diego <diegosantos4957@gmail.com>
2026-05-18 11:16:40 -0300

    feat: update CompanyRepository to use Companies class for improved consistency

--- src/main/java/com/load/balance/repositories/CompanyRepository.java
+++ src/main/java/com/load/balance/repositories/CompanyRepository.java
@@ -1,6 +1,6 @@
 package com.load.balance.repositories;
 
-import com.load.balance.models.Company;
+import com.load.balance.models.Companies;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface CompanyRepository extends JpaRepository<Company, UUID> {
-    @Query("SELECT c FROM Company c JOIN c.members u WHERE c.slug = :slug AND u.id = :userId")
-    List<Company> companiesBySlug(@RequestParam String slug, UUID userId);
+public interface CompanyRepository extends JpaRepository<Companies, UUID> {
+    @Query("SELECT c FROM Companies c JOIN c.members u WHERE c.slug = :slug AND u.id = :userId")
+    List<Companies> companiesBySlug(@RequestParam String slug, UUID userId);
 }
