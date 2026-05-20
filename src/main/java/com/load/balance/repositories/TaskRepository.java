e15bddb968c4504436ae4d28cc8eccebe4163255
Diego <diegosantos4957@gmail.com>
2026-05-20 19:05:26 -0300

    feat: rename Task model to Tasks in TaskRepository for consistency

--- src/main/java/com/load/balance/repositories/TaskRepository.java
+++ src/main/java/com/load/balance/repositories/TaskRepository.java
@@ -1,11 +1,11 @@
 package com.load.balance.repositories;
 
-import com.load.balance.models.Task;
+import com.load.balance.models.Tasks;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, UUID> {
+public interface TaskRepository extends JpaRepository<Tasks, UUID> {
 }
