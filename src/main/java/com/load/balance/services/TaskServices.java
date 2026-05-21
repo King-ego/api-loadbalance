7a746fe49ad88425eb0a7018ce17079883bdb469
Diego <diegosantos4957@gmail.com>
2026-05-21 19:29:57 -0300

    feat: add TaskRepository dependency and createTask method in TaskServices

--- src/main/java/com/load/balance/services/TaskServices.java
+++ src/main/java/com/load/balance/services/TaskServices.java
@@ -1,9 +1,18 @@
 package com.load.balance.services;
 
+import com.load.balance.repositories.TaskRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class TaskServices {
+    private final TaskRepository taskRepository;
+    public TaskServices(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public String createTask() {
+        return "Create Task";
+    }
 }
