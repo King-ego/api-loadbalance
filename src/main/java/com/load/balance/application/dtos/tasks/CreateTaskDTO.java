11215ed73348393cbd2e5bc2c0b44964712b79db
Diego <diegosantos4957@gmail.com>
2026-05-21 19:29:45 -0300

    feat: expand CreateTaskDTO with additional fields for task details

--- src/main/java/com/load/balance/application/dtos/tasks/CreateTaskDTO.java
+++ src/main/java/com/load/balance/application/dtos/tasks/CreateTaskDTO.java
@@ -1,4 +1,31 @@
 package com.load.balance.application.dtos.tasks;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class CreateTaskDTO {
+    private String name;
+
+    private String description;
+
+    private String status;
+
+    private Integer priority;
+
+    private UUID company_id;
+
+    private UUID member_id;
+
+    private int completedIn;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate startedAt;
 }
