8d10a91e5547acb07e699b054935da11bd4cec4d
Diego <diegosantos4957@gmail.com>
2026-05-21 19:29:51 -0300

    feat: implement createTask endpoint in TaskController using TaskServices

--- src/main/java/com/load/balance/controllers/TaskController.java
+++ src/main/java/com/load/balance/controllers/TaskController.java
@@ -1,4 +1,20 @@
 package com.load.balance.controllers;
 
+import com.load.balance.services.TaskServices;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+    private final TaskServices taskServices;
+
+    TaskController(TaskServices taskServices){
+        this.taskServices = taskServices;
+    }
+    @PostMapping
+    public String createTask() {
+        return this.taskServices.createTask();
+    }
 }
