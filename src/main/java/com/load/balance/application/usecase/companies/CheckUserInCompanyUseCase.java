3c6dd969d83c7887b659aebbb9d2694c503630fe
Diego <diegosantos4957@gmail.com>
2026-05-22 18:27:18 -0300

    feat: add getMember method to CheckUserInCompanyUseCase for member retrieval

--- src/main/java/com/load/balance/application/usecase/companies/CheckUserInCompanyUseCase.java
+++ src/main/java/com/load/balance/application/usecase/companies/CheckUserInCompanyUseCase.java
@@ -1,5 +1,6 @@
 package com.load.balance.application.usecase.companies;
 
+import com.load.balance.models.Member;
 import com.load.balance.repositories.MemberRepository;
 import org.springframework.stereotype.Component;
 
@@ -24,5 +25,8 @@ public class CheckUserInCompanyUseCase {
                     throw new RuntimeException("User is already a member of the company");
                 });
     }
-
+    public Member getMember(UUID userId, UUID companyId) {
+        return this.memberRepository.findMemberByUserIdAndCompanyId(userId, companyId)
+                .orElseThrow(() -> new RuntimeException("User is not a member of the company"));
+    }
 }
