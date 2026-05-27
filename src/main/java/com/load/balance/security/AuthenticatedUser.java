563ec8284ba664e2f328a2af27e32a5ea910a97c
Diego <diegosantos4957@gmail.com>
2026-05-27 17:25:06 -0300

    feat: implement AuthenticatedUser class to enhance user details management

--- src/main/java/com/load/balance/security/AuthenticatedUser.java
+++ src/main/java/com/load/balance/security/AuthenticatedUser.java
@@ -1,4 +1,40 @@
 package com.load.balance.security;
 
-public class AuthenticatedUser {
+import com.load.balance.enums.UserRoles;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class AuthenticatedUser
+        implements UserDetails, Serializable {
+
+    private UUID id;
+    private String email;
+    private UserRoles role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(
+                new SimpleGrantedAuthority(role.name())
+        );
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
