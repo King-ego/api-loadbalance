f0763271c8c7333fed1fecc91f959269767b1264
Diego <diegosantos4957@gmail.com>
2026-05-27 17:25:25 -0300

    feat: implement AuthHelper methods to retrieve authenticated user details

--- src/main/java/com/load/balance/application/helpers/auth/AuthHelper.java
+++ src/main/java/com/load/balance/application/helpers/auth/AuthHelper.java
@@ -1,4 +1,39 @@
 package com.load.balance.application.helpers.auth;
 
+import com.load.balance.models.Users;
+import com.load.balance.repositories.UserRepository;
+import com.load.balance.security.AuthenticatedUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
 public class AuthHelper {
+
+    private final UserRepository usersRepository;
+
+    public AuthenticatedUser getAuthenticatedUser() {
+        Authentication auth = SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        assert auth != null;
+
+        return (AuthenticatedUser) auth.getPrincipal();
+    }
+
+    public UUID getAuthenticatedUserId() {
+        return this.getAuthenticatedUser().getId();
+    }
+
+    public Users getSessionUser() {
+        return usersRepository
+                .findById(this.getAuthenticatedUserId())
+                .orElseThrow(() ->
+                        new RuntimeException("User not found"));
+    }
 }
