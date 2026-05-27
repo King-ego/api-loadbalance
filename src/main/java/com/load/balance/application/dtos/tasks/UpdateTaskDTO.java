66b957ad623500dcdc7feb28461ec2e60b13b929
Diego <diegosantos4957@gmail.com>
2026-05-27 16:22:31 -0300

    feat: expand UpdateTaskDTO with additional fields for task updates

--- src/main/java/com/load/balance/application/dtos/tasks/UpdateTaskDTO.java
+++ src/main/java/com/load/balance/application/dtos/tasks/UpdateTaskDTO.java
@@ -1,4 +1,16 @@
 package com.load.balance.application.dtos.tasks;
 
+import java.time.LocalDateTime;
+
 public class UpdateTaskDTO {
+    private String name;
+
+    private String description;
+
+    private String status;
+
+    private Integer priority;
+
+    private int completedIn;
+    private LocalDateTime completedAt;
 }
