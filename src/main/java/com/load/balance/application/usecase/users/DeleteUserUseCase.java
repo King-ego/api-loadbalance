73451109be6bc74c053b0270e12eff82bfbd139c
Diego <diegosantos4957@gmail.com>
2026-05-28 15:47:43 -0300

    feat: refactor DeleteUserUseCase to accept user object directly and remove unused dependency

--- src/main/java/com/load/balance/application/usecase/users/DeleteUserUseCase.java
+++ src/main/java/com/load/balance/application/usecase/users/DeleteUserUseCase.java
@@ -6,22 +6,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 public class DeleteUserUseCase {
     private final UserRepository userRepository;
-    private final FindUserOrThrowUseCase findUserOrThrowUseCase;
     private static final Logger log = LoggerFactory.getLogger(DeleteUserUseCase.class);
 
-    public DeleteUserUseCase(UserRepository userRepository, FindUserOrThrowUseCase findUserOrThrowUseCase) {
+    public DeleteUserUseCase(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.findUserOrThrowUseCase = findUserOrThrowUseCase;
     }
 
-    public void execute(UUID userId) {
-        Users user = findUserOrThrowUseCase.byId(userId);
+    public void execute(Users user) {
         userRepository.delete(user);
-        log.info("User deleted: {}", userId);
+        log.info("User deleted: {}", user.getId());
     }
 }
