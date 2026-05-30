4071bd476756e809709d10e8911cfcd150eb8416
Diego <diegosantos4957@gmail.com>
2026-05-30 20:19:56 -0300

    feat: implement update method in TaskServices

--- src/main/java/com/load/balance/services/TaskServices.java
+++ src/main/java/com/load/balance/services/TaskServices.java
@@ -4,6 +4,7 @@ import com.load.balance.application.dtos.tasks.CreateTaskDTO;
 import com.load.balance.application.dtos.tasks.UpdateTaskDTO;
 import com.load.balance.application.usecase.companies.CheckUserInCompanyUseCase;
 import com.load.balance.application.usecase.companies.FindCompanyOrThrowUseCase;
+import com.load.balance.application.usecase.tasks.FindTaskOrThrowUseCase;
 import com.load.balance.application.usecase.users.FindUserOrThrowUseCase;
 import com.load.balance.models.Member;
 import com.load.balance.models.Tasks;
@@ -24,12 +25,20 @@ public class TaskServices {
     private final FindUserOrThrowUseCase findUserOrThrowUseCase;
     private final FindCompanyOrThrowUseCase findCompanyOrThrowUseCase;
     private final CheckUserInCompanyUseCase checkUserInCompanyUseCase;
-    public TaskServices(TaskRepository taskRepository,  FindUserOrThrowUseCase findUserOrThrowUseCase,  FindCompanyOrThrowUseCase findCompanyOrThrowUseCase, CheckUserInCompanyUseCase checkUserInCompanyUseCase) {
+    private final FindTaskOrThrowUseCase findTaskOrThrowUseCase;
+
+    public TaskServices(
+            TaskRepository taskRepository,
+            FindUserOrThrowUseCase findUserOrThrowUseCase,
+            FindCompanyOrThrowUseCase findCompanyOrThrowUseCase,
+            CheckUserInCompanyUseCase checkUserInCompanyUseCase,
+            FindTaskOrThrowUseCase findTaskOrThrowUseCase
+    ) {
         this.taskRepository = taskRepository;
         this.findUserOrThrowUseCase = findUserOrThrowUseCase;
         this.findCompanyOrThrowUseCase = findCompanyOrThrowUseCase;
         this.checkUserInCompanyUseCase = checkUserInCompanyUseCase;
-
+        this.findTaskOrThrowUseCase = findTaskOrThrowUseCase;
     }
 
     public String createTask(CreateTaskDTO createTaskDTO, HttpSession session) {
@@ -61,7 +70,21 @@ public class TaskServices {
         return "Create Task";
     }
 
-    public String update(UpdateTaskDTO updateTaskDTO, HttpSession session) {
-        return "Update Task";
+    public Tasks update(UpdateTaskDTO updateTaskDTO, HttpSession session) {
+        String sessionUserIdStr = (String) session.getAttribute("userId");
+        UUID sessionUserId = UUID.fromString(sessionUserIdStr);
+
+        this.findUserOrThrowUseCase.byId(sessionUserId);
+
+        Tasks task = this.findTaskOrThrowUseCase.byId(updateTaskDTO.getTaskId());
+
+        if (updateTaskDTO.getName() != null) task.setName(updateTaskDTO.getName());
+        if (updateTaskDTO.getDescription() != null) task.setDescription(updateTaskDTO.getDescription());
+        if (updateTaskDTO.getStatus() != null) task.setStatus(updateTaskDTO.getStatus());
+        if (updateTaskDTO.getPriority() != null) task.setPriority(updateTaskDTO.getPriority());
+        if (updateTaskDTO.getCompletedIn() != null) task.setCompletedIn(updateTaskDTO.getCompletedIn());
+        if (updateTaskDTO.getCompletedAt() != null) task.setCompletedAt(updateTaskDTO.getCompletedAt());
+
+        return this.taskRepository.save(task);
     }
 }
