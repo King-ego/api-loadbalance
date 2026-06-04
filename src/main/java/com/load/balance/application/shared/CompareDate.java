1d6286e820945a4e3a210f4e400216121f5956bd
Diego <diegosantos4957@gmail.com>
2026-06-04 14:35:01 -0300

    chore: add comparison methods for LocalDateTime in CompareDate class

--- src/main/java/com/load/balance/application/shared/CompareDate.java
+++ src/main/java/com/load/balance/application/shared/CompareDate.java
@@ -1,4 +1,23 @@
 package com.load.balance.application.shared;
 
+import java.time.LocalDateTime;
+
 public class CompareDate {
+    public static void after(LocalDateTime date1, LocalDateTime date2) {
+        if (!date1.isAfter(date2)) {
+            throw new RuntimeException("Date1 not is after Date2");
+        }
+    }
+
+    public static void before(LocalDateTime date1, LocalDateTime date2) {
+        if (!date1.isBefore(date2)) {
+            throw new RuntimeException("Date1 not is before Date2");
+        }
+    }
+
+    public static void equals(LocalDateTime date1, LocalDateTime date2) {
+        if (!date1.isEqual(date2)) {
+            throw new RuntimeException("Date1 not equals Date2");
+        }
+    }
 }
