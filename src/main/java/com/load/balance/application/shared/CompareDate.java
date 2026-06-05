a7226dcafe5944b50968026c25775c74d9d5d7ca
Diego <diegosantos4957@gmail.com>
2026-06-05 18:48:18 -0300

    chore: refactor CompareDate methods to return boolean values instead of throwing exceptions

--- src/main/java/com/load/balance/application/shared/CompareDate.java
+++ src/main/java/com/load/balance/application/shared/CompareDate.java
@@ -3,21 +3,15 @@ package com.load.balance.application.shared;
 import java.time.LocalDateTime;
 
 public class CompareDate {
-    public static void after(LocalDateTime date1, LocalDateTime date2) {
-        if (!date1.isAfter(date2)) {
-            throw new RuntimeException("Date1 not is after Date2");
-        }
+    public boolean after(LocalDateTime date1, LocalDateTime date2) {
+        return date1.isAfter(date2);
     }
 
-    public static void before(LocalDateTime date1, LocalDateTime date2) {
-        if (!date1.isBefore(date2)) {
-            throw new RuntimeException("Date1 not is before Date2");
-        }
+    public boolean before(LocalDateTime date1, LocalDateTime date2) {
+        return date1.isBefore(date2);
     }
 
-    public static void equals(LocalDateTime date1, LocalDateTime date2) {
-        if (!date1.isEqual(date2)) {
-            throw new RuntimeException("Date1 not equals Date2");
-        }
+    public boolean equals(LocalDateTime date1, LocalDateTime date2) {
+        return date1.isEqual(date2);
     }
 }
