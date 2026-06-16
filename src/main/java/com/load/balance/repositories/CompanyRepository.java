baf77a303bc4a1eef72095f133a726db90d4466f
Diego <diegosantos4957@gmail.com>
2026-06-15 22:29:13 -0300

    feat: add findByCreatorId in  CompanyRepository

--- src/main/java/com/load/balance/repositories/CompanyRepository.java
+++ src/main/java/com/load/balance/repositories/CompanyRepository.java
@@ -7,10 +7,14 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Companies, UUID> {
     @Query("SELECT c FROM Companies c JOIN c.members u WHERE c.slug = :slug AND u.id = :userId")
     List<Companies> companiesBySlug(@RequestParam String slug, UUID userId);
+
+    @Query("SELECT c FROM Companies WHERE c.created_by_id = :creator_id")
+    Optional<Companies> findByCreatorId(@RequestParam UUID creatorId);
 }
